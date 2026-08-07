b041adf9b9486b6ae3a2837eddb99d1fbfcace85
Andrii Kovalenko <andrii.kovalenko@intellias.com>
2026-08-07 23:56:22 +0300

    feat: bootstrap Spring Boot application

---
+++ backend/app/src/main/java/com/andrii/vaultnote/app/VaultNoteApplication.java
@@ -0,0 +1,12 @@
+package com.andrii.vaultnote.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaultNoteApplication.class, args);
+    }
+}
